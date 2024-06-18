b8afc50b5b4b3383461a7b062bab7ffa4da8743d
DidinDinesh <155463888+DidinDinesh@users.noreply.github.com>
2024-06-18 22:20:16 +0530

    service and contact section

--- script.js
+++ script.js
@@ -33,3 +33,34 @@ const blurHeader = ()=> {
 }
 
 window.addEventListener("scroll", blurHeader)
+
+
+const contactForm = document.getElementById("contact-form"),
+contactMessage =document.getElementById("contact-message")
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // service / template / form / publickey
+
+    emailjs.sendForm("service_ce3busi","template_q17t5jw","#contact-form","m2bnzNZeDdOV95Bd3")
+    .then( () => {
+
+        // show sent messages
+        contactMessage.textContent = "Message sent Successfully"
+
+        // remove message after five seconds
+        setTimeout( () => {
+            contactMessage.textContent = ""
+        }, 5000)
+
+        //clear input fields
+        contactForm.reset() 
+    }, 
+    () => {
+        //show error message
+        contactMessage.textContent = "messege not sent (service error)"
+    })
+}
+
+contactForm.addEventListener("submit", sendEmail)
